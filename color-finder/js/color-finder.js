f71a4001a1525367b547e6a35c9c1e0b3cdfaa7c
Lightsyde <taimande@gmail.com>
2012-07-04 16:45:10 -0400

    removed unnecessary variable

--- color-finder/js/color-finder.js
+++ color-finder/js/color-finder.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 		var changetoHSL = $.farbtastic('#colorpicker').hsl;
 		var hue = changetoHSL[0];
 		var compliment = 0;
-		var supplement = 0;
+		
 		
 		if (hue < .5) {
 			compliment = hue + .5;
