68ed659fcbe3c1c9d8e03c63431e700f5994c198
Lightsyde <taimande@gmail>
2012-07-06 15:42:25 -0400

    trying to get the value of h in hsl when measured from 0 to 100 instead of from 0 to 360...

--- color-finder/js/color-finder.js
+++ color-finder/js/color-finder.js
@@ -25,6 +25,7 @@ $(document).ready(function() {
 			, Math.round(changetoHSL[2] * 100)
 		]
 				
+		
 		console.log('hsl(' + hsl[0] + ', ' + hsl[1] + '%, ' + hsl[2] + '%)');
 		$('#compliment').css('background-color', 'hsl(' + hsl[0] + ', ' + hsl[1] + '%, ' + hsl[2] + '%)');
 		
@@ -37,11 +38,11 @@ $(document).ready(function() {
 		var trueHexa = hsl[0] 
 		var trueHexb = hsl[1]
 		var trueHexc = hsl[2]
-		console.log(trueHexb);
+		//console.log(trueHexb);
 		
 		//$.farbtastic('#colorpicker')$.farbtastic('#complimentary',trueHex);
 		
-		var trueHex = $.colors( 'hsla(98,trueHexb + '%',trueHexc + '%',0.4)' ).toString('hex');
+		var trueHex = $.colors( 'hsla((trueHexa / 3.6),trueHexb + '%',trueHexc + '%',1)' ).toString('hex');
 		$('#complimentary').val(trueHex);
 		console.log(trueHex);
 		console.log('hexCom(' + hexCom[0] + ', ' + hexCom[1] + ', ' + hexCom[2] + ')');
