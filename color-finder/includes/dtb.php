f5b6a41fc1e426112aed6f58f8a5394c1de67217
Lightsyde <taimande@gmail.com>
2012-08-08 15:05:12 -0400

    database

--- color-finder/includes/dtb.php
+++ color-finder/includes/dtb.php
@@ -1,9 +1,12 @@
 <?php
 
 
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$data_source = getenv('DATA_SOURCE');
+$user = getenv('MYSQL_USERNAME');
+$pass = getenv('MYSQL_PASSWORD');
+$host = getenv('MYSQL_DB_HOST');
+$dbname = getenv('MYSQL_DB_NAME');
+
+$data_source = sprintf('mysql:host=%s;dbname=%s', $host, $dbname);
 
 
 
